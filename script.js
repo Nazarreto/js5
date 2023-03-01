07fc28fb750d7ea63380758d801d0d27fe8d0425
Nazarreto <102985652+Nazarreto@users.noreply.github.com>
2023-03-01 21:05:02 +0200

    qweqwe

---
+++ script.js
@@ -0,0 +1,54 @@
+// Task 1
+
+// let monthNumber = +prompt("Введіть номер місяця:");
+
+// switch (monthNumber) {
+//   case 1:
+//   case 2:
+//   case 12:
+//     alert("Зима");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     alert("Весна");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     alert("Літо");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     alert("Осінь");
+//     break;
+//   default:
+//     alert("Це неможливо");
+//     break;
+// }
+
+// task 2
+
+// function q(num) {
+//   for (let i = 2; i <= num; i++) {
+//     if (num % i === 0) {
+//       return `число непросте`;
+//     }
+//     return `число просте`;
+//   }
+// }
+// const result = q(19);
+// console.log(result);
+
+// task 3
+
+// function max(a, b, c) {
+//   if (a > b && a > c) {
+//     console.log(a);
+//   } else if (b > a && b > c) {
+//     console.log(b);
+//   } else console.log(c);
+// }
+
+// max(43, 88, 66);
